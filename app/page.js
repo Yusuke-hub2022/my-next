b7ad6f6e5af011ff6d90be82a2ef0529817f70e1
FirstName LastName <lastname_firstname@example.com>
2024-10-06 02:55:39 +0900

    add title, "Book List" in Home page

--- app/page.js
+++ app/page.js
@@ -4,11 +4,15 @@ import LinkedBookDetails from '@/components/LinkedBookDetails';
 export const dynamic = 'force-dynamic';
 export default async function Home() {
   const reviews = await getAllReviews();
-  if (reviews.length === 0) {
-    return <p className="text-black">登録されている本はありません。Searchページから本を選んでください。</p>
-  }
+
   return (
     <>
+    <p className="mb-3 text-black text-2xl">Book List</p>
+    {
+      reviews.length === 0
+      ? <p className="text-black">登録されている本はありません。Searchページから本を選んでください。</p>
+      : ''
+    }
     {reviews.map((b, i) => (
       <LinkedBookDetails book={b} index={i + 1} key={b.id} />
     ))}
